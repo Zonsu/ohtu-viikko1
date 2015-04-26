5e60ccdf4cdef3188e75bc591d0bb74c3d736c5d
yosq <johanna.paavola@helsinki.fi>
2015-04-26 23:20:25 +0300

    tehtävä 4

--- ohtu-viikko6/Tennis/src/ohtu/TennisGame.java
+++ ohtu-viikko6/Tennis/src/ohtu/TennisGame.java
@@ -1,9 +1,9 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,67 +13,77 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == "player1") {
+            player1Scores();
+        } else {
+            player2Scores();
+        }
+    }
+
+    public void player1Scores() {
+        player1Score += 1;
+    }
+
+    public void player2Scores() {
+        player2Score += 1;
+    }
+
+    public String getTiedScore(int score) {
+        String scoreMessages[] = {"Love-All", "Fifteen-All", "Thirty-All", "Forty-All", "Deuce"};
+        return scoreMessages[score];
+    }
+
+    public String getAdvantageMessage() {
+
+        int playerScoreDifference = player1Score - player2Score;
+        if (playerScoreDifference == 1) {
+            return "Advantage player1";
+        } else {
+            return "Advantage player2";
+        }
+    }
+
+    public String getWinMessage() {
+
+        int playerScoreDifference = player1Score - player2Score;
+        if (playerScoreDifference >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+
+    public String playerMessages(int msgIndex) {
+        String scoreMessages[] = {"Love", "Fifteen", "Thirty", "Forty"};
+        return scoreMessages[msgIndex];
+    }
+
+    public String getPlayer1Message() {
+        return playerMessages(player1Score);
+    }
+
+    public String getPlayer2Message() {
+        return playerMessages(player2Score);
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
+
+        if (player1Score == player2Score) {
+            score = getTiedScore(player1Score);
+
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            int playerScoreDifference = Math.abs(player1Score - player2Score);
+
+            if (playerScoreDifference < 2) {
+                score = getAdvantageMessage();
+            } else {
+                score = getWinMessage();
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+
+        } else {
+            score = getPlayer1Message() + "-" + getPlayer2Message();
+
         }
         return score;
     }
