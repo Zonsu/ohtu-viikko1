7967cbce1e9daf6a43e93ea6bf540de1c0a712f5
yosq <johanna.paavola@helsinki.fi>
2015-04-26 23:44:40 +0300

    tehtävä 4

--- ohtu-viikko6/QueryLanguage1/src/main/java/statistics/Main.java
+++ ohtu-viikko6/QueryLanguage1/src/main/java/statistics/Main.java
@@ -3,16 +3,31 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
+
+        Matcher m = new And(new HasAtLeast(10, "goals"),
+                new HasAtLeast(10, "assists"),
+                new PlaysIn("PHI")
         );
-        
+        Matcher m2 = new Or(new HasAtLeast(40, "goals"),
+                new HasAtLeast(60, "assists")
+        );
+        Matcher m3 = new Not(new HasFewerThan(40, "goals"),
+                new PlaysIn("PHI")
+        );
+
         for (Player player : stats.matches(m)) {
-            System.out.println( player );
+            System.out.println(player);
+        }
+        System.out.println("");
+        for (Player player : stats.matches(m2)) {
+            System.out.println(player);
+        }
+        System.out.println("");
+        for (Player player : stats.matches(m3)) {
+            System.out.println(player);
         }
     }
 }
---
+++ ohtu-viikko6/QueryLanguage1/src/main/java/statistics/matcher/Or.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics.matcher;
+
+import statistics.Player;
+
+/**
+ *
+ * @author Johanna
+ */
+public class Or implements Matcher {
+
+    private Matcher[] matchers;
+
+    public Or(Matcher... matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        int matches = 0;
+        
+        for (Matcher matcher : matchers) {
+            if (matcher.matches(p)) {
+                matches++;
+            }
+        }
+        if (matches == 0) {
+            return false;
+        }
+        return true;
+    }
+}
